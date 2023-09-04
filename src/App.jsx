1f7836f0829db2ca12cb9e24696b59ee728a9747
Anthony Lines <linesanthony018@gmail.com>
2023-09-04 18:28:21 +0200

    fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,40 @@
-function App() {
- 
-  return (
+import loader from "./assets/loader.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+const APIKEY = import.meta.env.VITE_WEATHER_API_KEY;
 
-      <main>
-      
-      </main>
-  
+function App() {
+  const [weatherData, setWeatherData] = useState(null);
+
+  useEffect(() => {
+    fetch(`http://api.airvisual.com/v2/nearest_city?key=${APIKEY}`)
+      .then((response) => {
+        console.log(response);
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setWeatherData({
+          city: responseData.data.city,
+          country: responseData.data.country,
+          iconId: responseData.data.current.weather.ic,
+          temperature: responseData.data.current.weather.tp,
+        });
+      });
+  }, []);
+
+  return (
+    <main>
+      <div classname="loader-container">
+        <img src={loader} alt="loading icon" />
+      </div>
+      <p className="city-name">Mont de marsan</p>
+      <p className="country-name">France</p>
+      <p className="temperature">17</p>
+      <div className="info-icon-container">
+        <img src="/icons/01d.svg" className="info-icon" alt="weather icon" />
+      </div>
+    </main>
   );
 }
 
